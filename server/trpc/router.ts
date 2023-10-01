27ade1b57d728574e4a8819390aadbbe2a59d08d
Piyush <bhangalepiyush@gmail.com>
2023-10-01 11:53:27 +0530

    feat(tRPC): ad github router

--- server/trpc/router.ts
+++ server/trpc/router.ts
@@ -1,6 +1,8 @@
+import { githubRouter } from './routers/github'
 import { router } from './trpc'
 
 export const appRouter = router({
+  githubRouter,
 })
 
 export type AppRouter = typeof appRouter
---
+++ server/trpc/routers/github.ts
@@ -0,0 +1,20 @@
+import { Octokit } from 'octokit'
+import { protectedProcedure, router } from '../trpc'
+
+export const githubRouter = router({
+  getRepositories: protectedProcedure
+    .query(async ({ ctx }) => {
+      const user = ctx.user.user
+
+      const octokit = new Octokit({
+        auth: user.token,
+      })
+
+      const { data } = await octokit.rest.repos.listForAuthenticatedUser({
+        sort: 'updated',
+        type: 'owner',
+      })
+
+      return data
+    }),
+})
