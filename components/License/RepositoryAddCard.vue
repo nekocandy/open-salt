5b4dbac8cf0cdbda562fdceee1664eb8d28e0f16
Piyush <bhangalepiyush@gmail.com>
2023-10-01 12:54:32 +0530

    feat: add single click license addition from dashboard

--- components/License/RepositoryAddCard.vue
+++ components/License/RepositoryAddCard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { ILicenseData } from '~/lib/types/License'
 
-defineProps<{
+const props = defineProps<{
   repository: RepositoryData[0]
   licenseData: ILicenseData
 }>()
@@ -9,13 +9,28 @@ defineProps<{
 const { $client } = useNuxtApp()
 
 type RepositoryData = Awaited<ReturnType<typeof $client.githubRouter.getRepositoryWithoutLicense.query>>
+
+async function addLicense() {
+  // eslint-disable-next-line no-alert
+  const toContinue = confirm(`Are you sure you want to add the ${props.licenseData.title} license to ${props.repository.name}?`)
+  if (!toContinue)
+    return
+
+  await $client.githubRouter.addLicense.mutate({
+    repositoryName: props.repository.name,
+    license: props.licenseData.text,
+  })
+
+  // eslint-disable-next-line no-alert
+  alert(`Successfully added the ${props.licenseData.title} license to ${props.repository.name}`)
+}
 </script>
 
 <template>
-  <div flex items-center gap-2 rounded-md bg-white px-4 py-2>
+  <button w-full flex items-center gap-2 rounded-md bg-white px-4 py-2 @click="addLicense">
     <div i-tdesign-git-repository />
     {{ repository.name }}
-  </div>
+  </button>
 </template>
 
 <style scoped>
