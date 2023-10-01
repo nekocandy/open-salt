9db3498ed0944f9003224e41497c0004a3fcab3a
Piyush <bhangalepiyush@gmail.com>
2023-10-01 13:16:37 +0530

    feat(tRPC): route to check if code of conduct exists or not

--- server/trpc/routers/github.ts
+++ server/trpc/routers/github.ts
@@ -35,6 +35,52 @@ export const githubRouter = router({
       return data.filter(repo => repo.license == null)
     }),
 
+  getRepositoryWithoutCodeOfConduct: protectedProcedure
+    .query(async ({ ctx }) => {
+      const user = ctx.user.user
+
+      const octokit = new Octokit({
+        auth: user.token,
+      })
+
+      const { data } = await octokit.rest.repos.listForAuthenticatedUser({
+        sort: 'updated',
+        type: 'owner',
+      })
+
+      const fileTrees = await Promise.allSettled(
+        data.map(async (repo) => {
+          const { data } = await octokit.rest.git.getTree({
+            owner: user.username,
+            repo: repo.name,
+            tree_sha: 'HEAD',
+            recursive: 'true',
+          })
+
+          return data
+        }),
+      )
+
+      const locations = [
+        'CODE_OF_CONDUCT',
+        'docs/CODE_OF_CONDUCT',
+        '.github/CODE_OF_CONDUCT',
+      ]
+
+      const withoutCodeOfConduct = fileTrees.filter((fileTree) => {
+        if (fileTree.status === 'rejected')
+          return false
+
+        return locations.every((location) => {
+          return !fileTree.value.tree.filter(file => file.path).some((file) => {
+            return file.path!.includes(location)
+          })
+        })
+      })
+
+      return withoutCodeOfConduct
+    }),
+
   addLicense: protectedProcedure
     .input(z.object({
       repositoryName: z.string(),
