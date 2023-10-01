79ba8bdc3f33ef2f37792f393e3301a0ddafdd41
Piyush <bhangalepiyush@gmail.com>
2023-10-01 12:53:03 +0530

    feat(tRPC): add route to commit data to file

--- server/trpc/routers/github.ts
+++ server/trpc/routers/github.ts
@@ -1,4 +1,5 @@
 import { Octokit } from 'octokit'
+import { z } from 'zod'
 import { protectedProcedure, router } from '../trpc'
 
 export const githubRouter = router({
@@ -32,5 +33,31 @@ export const githubRouter = router({
       })
 
       return data.filter(repo => repo.license == null)
+    }),
+
+  addLicense: protectedProcedure
+    .input(z.object({
+      repositoryName: z.string(),
+      license: z.string(),
+    }))
+    .mutation(async ({ ctx, input }) => {
+      const user = ctx.user.user
+
+      const octokit = new Octokit({
+        auth: user.token,
+      })
+
+      await octokit.rest.repos.createOrUpdateFileContents({
+        owner: user.username,
+        repo: input.repositoryName,
+        path: 'LICENSE',
+        message: 'docs: add license | added from website',
+        // eslint-disable-next-line n/prefer-global/buffer
+        content: Buffer.from(input.license).toString('base64'),
+        author: {
+          name: 'Nekocandy Bot',
+          email: 'saucebot@nekocandy.club',
+        },
+      })
     }),
 })
