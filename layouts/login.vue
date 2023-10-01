c2b15ff0a13434bdc953fe49f7fe1cca2137117c
Piyush <bhangalepiyush@gmail.com>
2023-10-01 09:47:42 +0530

    feat: add login page

---
+++ layouts/login.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import BackgroundImage from '~/assets/bg.jpg'
+</script>
+
+<template>
+  <div
+
+    :style="{
+      backgroundImage: `url(${BackgroundImage})`,
+    }"
+    h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'login',
+})
+
+const { signIn, status } = useAuth()
+
+async function login() {
+  if (status.value === 'authenticated')
+    return navigateTo('/dashboard')
+
+  await signIn('github')
+}
+</script>
+
+<template>
+  <div flex flex-col items-center justify-center gap-8 rounded-md bg-white bg-opacity-20 px-8 py-8 font-mono>
+    <h1 text-3xl font-black uppercase>
+      Open Source~
+    </h1>
+
+    <button flex items-center justify-center gap-2 rounded-md bg-white px-12 py-4 text-lg font-bold class="hover:bg-opacity-90" @click="login">
+      <div i-simple-icons-github h-6 w-6 />
+      Login with Github
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
