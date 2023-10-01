cf869096b51f676c37425e7dcd4649bd167f415c
Piyush <bhangalepiyush@gmail.com>
2023-10-01 13:43:01 +0530

    feat: initial code of conduct page

---
+++ pages/code-of-conduct.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div flex flex-col gap-4>
+    <h1 text-4xl font-black uppercase>
+      Code of Conduct:
+    </h1>
+
+    <div flex flex-col gap-4 rounded-md bg-yellow-200 px-12 py-6 text-center text-2xl font-extrabold font-sans text-gray-700>
+      <q>
+        A code of conduct that isn’t (or can’t be) enforced is worse than no code of conduct at all: it sends the message that the values in the code of conduct aren’t actually important or respected in your community.
+      </q>
+
+      <span text-right>
+        ~Ada Initiative
+      </span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
