3aa26dd9b5ba7f5b01114271c6e31c5691ce040b
Piyush <bhangalepiyush@gmail.com>
2023-10-01 09:35:20 +0530

    chore: test auth

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
+const { signIn, status, signOut } = useAuth()
 
+async function login() {
+  if (status.value === 'authenticated')
+    await signOut()
+
+  await signIn('github')
+}
 </script>
 
 <template>
   <div class="h-screen flex items-center justify-center bg-zinc-900 font-mono text-white">
-    Hello, Pizza!
+    <button @click="login">
+      Login with WitHub
+    </button>
   </div>
 </template>
 
