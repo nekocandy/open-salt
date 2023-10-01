ae4d54e9823169486d69c14f2717a208a3d11a1d
Piyush <bhangalepiyush@gmail.com>
2023-10-01 11:43:01 +0530

    feat: make more changes to license page

--- pages/license/info/[license].vue
+++ pages/license/info/[license].vue
@@ -17,8 +17,8 @@ async function copyToClipboard() {
 </script>
 
 <template>
-  <div flex gap-4>
-    <div w-full flex flex-col gap-4>
+  <div grid grid-cols-12 w-full gap-4>
+    <div col-span-8 w-full flex flex-col gap-4>
       <div flex flex-col gap-1>
         <h1 text-4xl font-black uppercase>
           {{ data.title }}
@@ -78,11 +78,27 @@ async function copyToClipboard() {
       </div>
     </div>
 
-    <div>
-      <button flex items-center gap-2 rounded-md bg-blue px-4 py-2 text-nowrap text-black @click="copyToClipboard">
+    <div col-span-3 w-full flex flex-col gap-4>
+      <button flex items-center justify-center gap-2 rounded-md bg-blue px-4 py-2 text-nowrap text-black @click="copyToClipboard">
         <div i-mi-copy h-6 w-6 />
         Copy License to Clipboard
       </button>
+
+      <div border-b-2 border-stone-400 />
+
+      <div flex flex-col gap-4>
+        <h1 font-bold uppercase>
+          Note:
+        </h1>
+
+        <div>
+          {{ data.note || 'No note provided.' }}
+        </div>
+      </div>
+
+      <div border-b-2 border-stone-400 />
+
+      <div border-b-2 border-stone-400 />
     </div>
   </div>
 </template>
