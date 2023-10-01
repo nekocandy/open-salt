7c56bc478d3004cc3355706801794691a3ea01bd
Piyush <bhangalepiyush@gmail.com>
2023-10-01 09:50:19 +0530

    feat: default layout and initial dashboard page

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
-
+import BackgroundImage from '~/assets/bg.jpg'
 </script>
 
 <template>
-  <div>
-    <slot />
+  <div
+
+    :style="{
+      // backgroundImage: `url(${BackgroundImage})`,
+    }"
+
+    h-screen flex flex-col gap-4 bg-yellow-100 bg-cover bg-center bg-no-repeat
+    px-4 py-4
+  >
+    <div />
+
+    <div flex-1>
+      <slot />
+    </div>
   </div>
 </template>
 
